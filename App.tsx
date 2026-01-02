62fb1e16bc4760f1091fdc43a8574b429cf35ebf
Chinmaya <chinmayamurari12@gmail.com>
2026-01-02 19:33:24 +0530

    added reanimated and react nativ gesture handler with a test animation to test its working

--- App.tsx
+++ App.tsx
@@ -6,7 +6,9 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StatusBar, StyleSheet, TouchableOpacity, useColorScheme, View } from 'react-native';
+import { GestureHandlerRootView, Text } from 'react-native-gesture-handler';
+import Animated,{useAnimatedStyle, useSharedValue, withSpring,} from 'react-native-reanimated';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
@@ -18,16 +20,32 @@ function App() {
   return (
     <SafeAreaProvider>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <GestureHandlerRootView style={{ flex: 1 }}>
       <AppContent />
+      </GestureHandlerRootView>
     </SafeAreaProvider>
   );
 }
 
 function AppContent() {
   const safeAreaInsets = useSafeAreaInsets();
+  const offset = useSharedValue(0);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: offset.value }],
+    };
+  });
 
   return (
     <View style={styles.container}>
+      <TouchableOpacity onPress={() => {
+        offset.value = withSpring(offset.value + 100);
+      }}>
+        <Animated.View style={[styles.box,animatedStyle]}>
+        <Text>Click me</Text>
+        </Animated.View>
+      </TouchableOpacity>
       <NewAppScreen
         templateFileName="App.tsx"
         safeAreaInsets={safeAreaInsets}
@@ -40,6 +58,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  box:{marginVertical:50,
+    borderRadius:12,
+    height:100,
+    width:100,
+     backgroundColor:'red',
+     justifyContent:'center',
+     alignItems:'center'}
 });
 
 export default App;
