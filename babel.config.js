bd59fdb0fe729e06a1c4cb7638df34583bed246f
Chinmaya <chinmayamurari12@gmail.com>
2026-01-02 20:25:46 +0530

    added react compilr for per imporvemnts  durign build creation

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
+    ['babel-plugin-react-compiler', {
+      // Optional: Add compiler options here
+      // For example, to enable compilation warnings:
+      // compilationMode: 'annotation',
+    }],
     'react-native-reanimated/plugin',
   ],
 };
